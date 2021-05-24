7c32df49f125217571d4d504a12ad3f061190171
Jairo Mejia <jmejia@efe.com>
2021-05-23 22:38:02 -0400

    game type new flow

--- app/firebase_utils/db.py
+++ app/firebase_utils/db.py
@@ -4,7 +4,13 @@ import datetime
 
 # Configuration goes here !!!
 firebase_config = {
-
+  "apiKey": "AIzaSyAqruys1IIGxdu9yOIIvxM6dAuPT_Y4Cic",
+  "authDomain": "ponzi-b40a8.firebaseapp.com",
+  "databaseURL": "https://ponzi-b40a8-default-rtdb.firebaseio.com",
+  "projectId": "ponzi-b40a8",
+  "storageBucket": "ponzi-b40a8.appspot.com",
+  "messagingSenderId": "475670648946",
+  "appId": "1:475670648946:web:70d30ab9a2dc86846a4aeb"
 }
 
 
