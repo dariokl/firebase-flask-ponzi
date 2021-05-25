3625d166905217aaa45dc16d01595bb0d4ecd5d1
Jairo Mejia <jmejia@efe.com>
2021-05-25 16:15:39 -0400

    changes in game reverse table 2

--- app/static/js/reverse.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $("tbody .results").each(function(elem,index){
-      var arr = $.makeArray($("tr",this).detach());
-      arr.reverse();
-        $(this).append(arr);
-    });
-});
